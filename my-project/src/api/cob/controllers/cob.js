f6d0c8eb0696799de296897b2ec7c0e6d7ed1fca
ketymahavi <kety.m@next-insurance.co>
2022-11-15 11:01:00 +0200

    add cob

---
+++ my-project/src/api/cob/controllers/cob.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cob controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cob.cob');
---
+++ my-project/src/api/cob/routes/cob.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cob router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cob.cob');
